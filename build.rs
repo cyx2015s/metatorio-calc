d5b1be0d43130e06cc324513bd91e3ad34d24bbe
cyx2015s <miracle041206@163.com>
2026-01-26 16:06:42 +0800

    build: 只考虑 windows 平台的构建了

--- build.rs
+++ build.rs
@@ -18,4 +18,11 @@ fn main() {
         format!("pub const GIT_HASH: &str = \"{}\";", git_hash),
     )
     .unwrap();
+
+    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        winresource::WindowsResource::new()
+            // This path can be absolute, or relative to your crate root.
+            .set_icon("assets/icon.ico")
+            .compile().unwrap();
+    }
 }
